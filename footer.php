3e44d98a938c5f7882217f21e6f5abdc68fddcf8
Karol <codemasterkarol@gmail.com>
2016-01-30 22:18:46 -0600

    Initial Commit

---
+++ footer.php
@@ -0,0 +1,13 @@
+
+</section>
+
+<footer>
+    <div class="row">
+        <div class="small-12 columns">
+            <p class="text-right">&copy  <script>document.write(new Date().getFullYear())</script> <a href="/">Cornerstone Hosting</a></p>
+        </div>
+    </div>
+</footer>
+
+</body>
+</html>
---
+++ header.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Cornerstone Hosting</title>
+    <link rel="stylesheet" type="text/css" href="css/foundation-icons.css">
+    <link rel="stylesheet" type="text/css" href="css/foundation.css">
+    <link rel="stylesheet" type="text/css" href="css/app.css">
+</head>
+<body>
+
+<nav>
+    <div class="row">
+        <div class="small-12 columns">
+            <ul class="menu">
+                <li style="float: left;"><a href="/"><img src="img/logo-text-only-white.svg" alt="Cornerstone"></a></li>
+                <li><a href="">Features</a></li>
+                <li><a href="">Pricing</a></li>
+                <li><a href="">Contact</a></li>
+            </ul>
+        </div>
+    </div>
+</nav>
+
+<header>
+    <div class="row">
+        <div class="small-12 columns">
+            <img src="img/logo-centered.svg" alt="Cornerstone Hosting Logo">
+            <h1>Cornerstone Hosting</h1>
+        </div>
+    </div>
+</header>
+<div class="arrow-down"></div>
