07d8028773784185a6d39bc12819509645c18c60
Karol Brennan <codemasterkarol@gmail.com>
2016-03-24 20:58:54 -0500

    Fixing the contact form

--- scripts/mailer.php
+++ scripts/mailer.php
@@ -27,17 +27,15 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $responseKeys = json_decode($response,true);
 
     if(intval($responseKeys["success"]) !== 1) {
-       echo "<img src='../img/spam.jpeg' alt='Spammer! GTFO!'>";
-    } else {
-        echo '<h2>Thanks for posting comment.</h2>';
+        echo "Spammer! Get out!";
+        exit;
     }
 
-
     // Check that data was sent to the mailer.
-    if ( empty($name) OR empty($message) OR !filter_var($email, FILTER_VALIDATE_EMAIL)) {
+    if ( empty($name) OR empty($message) OR $responseKeys === null OR !filter_var($email, FILTER_VALIDATE_EMAIL)) {
         // Set a 400 (bad request) response code and exit.
         http_response_code(400);
-        echo "Something is wrong with the data. It's not matching.";
+        echo "No can do buddy. Go try again!";
         exit;
     }
 
@@ -59,17 +57,17 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     if (mail($recipient, $subject, $email_content, $email_headers)) {
         // Set a 200 (okay) response code.
         http_response_code(200);
-        echo "Thank You! Your message has been sent.";
+        echo "Thank You! Your message has been sent and we will get back to you shortly!";
     } else {
         // Set a 500 (internal server error) response code.
         http_response_code(500);
-        echo "Ah shit it couldn't send the email.";
+        echo "Uh oh. Something went wrong!";
     }
 
 } else {
     // Not a POST request, set a 403 (forbidden) response code.
     http_response_code(403);
-    echo "FORBIDDEN!";
+    echo "THOU SHALT NOT EMAIL!";
 }
 
 ?>
